73051d35fef77abb36fb968ea72dd76ed410f6cc
Carlos Felix <carlosdrfx@gmail.com>
2024-10-17 19:13:36 +0100

    hello

---
+++ pontos_guardados.sql
@@ -0,0 +1,40 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 16.4
+-- Dumped by pg_dump version 16.4
+
+-- Started on 2024-10-03 21:41:35
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- TOC entry 5699 (class 0 OID 36430)
+-- Dependencies: 223
+-- Data for Name: gps_ponto; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO public.gps_ponto VALUES (1, 1, '0101000020E610000000000000000000000000000000000000');
+INSERT INTO public.gps_ponto VALUES (2, 3, '0101000020E6100000E8512FAA476AB73F0083A151E3C4A43F');
+INSERT INTO public.gps_ponto VALUES (4, 5, '0101000020E6100000C01483A151E3C83F40BF707EDA289BBF');
+INSERT INTO public.gps_ponto VALUES (7, 3, '0101000020E6100000E807B8492B7BCD3FC02CA4AA39C2CBBF');
+INSERT INTO public.gps_ponto VALUES (22, NULL, '0101000020E6100000C083391C499390BF0222ED9777ACD1BF');
+INSERT INTO public.gps_ponto VALUES (43, 3, '0101000020E6100000486B26BB94D9C0BF1CF03CB3DC8FC9BF');
+
+
+-- Completed on 2024-10-03 21:41:36
+
+--
+-- PostgreSQL database dump complete
+--
+
